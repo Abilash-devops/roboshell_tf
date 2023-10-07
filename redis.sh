5708b3baf9aede2fbd3eb3e72fe6cf7037b18a7d
Abilash-devops <141636108+Abilash-devops@users.noreply.github.com>
2023-10-07 10:50:26 +0530

    Update redis.sh

--- redis.sh
+++ redis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 D=$(date +%F:%H:%M:%S)
 SCRIPT_NAME=$0
-LOG_PATH=/home/centos/roboshell/logs
+LOG_PATH=/home/centos/roboshell_tf/logs
 LOGFILE=$LOG_PATH/$0-$D-log
 u=$(id -u)
 R="\e[31m"
